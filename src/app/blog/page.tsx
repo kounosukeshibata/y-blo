ccbc20b90a98d305f711ffc19217a3bbbd125df1
shibata_konosuke <shibata_k@r.recruit.co.jp>
2025-02-15 01:07:49 +0900

    Blogページの記事一覧の表示レイアウトを修正

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -23,30 +23,50 @@ const Page = async () => {
   return (
     <main>
       <Container>
-        <h1 className="mb-12 mt-10 text-5xl font-bold leading-tight tracking-tighter md:leading-none md:text-5xl lg:text-5xl text-center md:text-left">
+        <h1 className="mb-12 mt-10 xl:mx-20 text-5xl font-bold leading-tight tracking-tighter md:leading-none md:text-5xl lg:text-5xl text-center md:text-left">
           ブログ一覧ページ
         </h1>
-        {joinedTopicsPostData ? (
-          joinedTopicsPostData.map((article) => {
-            return (
-              <Link
-                href={`/blog/posts/${article.id}`}
-                className="hover:underline"
-                key={article.id}
-              >
-                <ul className="my-5">
-                  <li>
-                    {article.emoji} {article.title}
-                  </li>
-                  <li>Topics：{article.topics}</li>
-                  <li>投稿日：{article.published_at}</li>
-                </ul>
-              </Link>
-            )
-          })
-        ) : (
-          <p>No article.</p>
-        )}
+        <div className="grid grid-col sm:grid-cols-2 lg:grid-cols-3 xl:mx-20">
+          {sortedPostData ? (
+            sortedPostData.map((article) => {
+              return (
+                <div
+                  key={article.id}
+                  className="mx-10 sm:mx-5 px-5 my-5 sm:my-5 xl:mx-5 bg-sky-500/20 rounded-xl"
+                >
+                  <Link
+                    href={`/blog/posts/${article.id}`}
+                    className="hover:underline"
+                  >
+                    <ul className="my-2 flex flex-col h-full">
+                      <li className="mt-2 mb-4 px-3 py-2 bg-white rounded-xl">
+                        {article.emoji} {article.title}
+                      </li>
+                      <li className="flex flex-wrap py-2">
+                        キーワード：
+                        {article.topics.map((topic, index) => {
+                          return (
+                            <div
+                              key={index}
+                              className="my-0.5 mx-0.5 px-2 bg-blue-900 text-white rounded-xl"
+                            >
+                              {topic}
+                            </div>
+                          )
+                        })}
+                      </li>
+                      <li className="py-2 mt-auto mb-3 self-end">
+                        投稿日：{article.published_at}
+                      </li>
+                    </ul>
+                  </Link>
+                </div>
+              )
+            })
+          ) : (
+            <p>No article.</p>
+          )}
+        </div>
       </Container>
     </main>
   )
