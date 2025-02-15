d161be39bb21e59327e07bd43ee9989952fe3a0f
shibata_konosuke <shibata_k@r.recruit.co.jp>
2025-02-16 00:21:53 +0900

    固定ヘッダーにページのタイトルが隠れてしまうバグを修正

--- src/app/_components/MailContact.tsx
+++ src/app/_components/MailContact.tsx
@@ -81,36 +81,6 @@ const MailContact = () => {
     }
   }
 
-  // const router = useRouter()
-  // const [name, setName] = useState('')
-  // const [email, setEmail] = useState('')
-  // const [subject, setSubject] = useState('')
-  // const [message, setMessage] = useState('')
-
-  // const handleSubmit = async (e: React.FormEvent) => {
-  //   e.preventDefault()
-
-  //   const res = await fetch('/api/email', {
-  //     method: 'POST',
-  //     headers: {
-  //       Accept: 'application/json, text/plain',
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({ name, email, subject, message }),
-  //   })
-
-  //   if (res.status === 200) {
-  //     console.log('メール送信成功')
-  //     setName('')
-  //     setEmail('')
-  //     setSubject('')
-  //     setMessage('')
-  //     router.push('/contact')
-  //   } else {
-  //     console.error('メール送信に失敗しました')
-  //   }
-  // }
-
   return (
     <div className="container bg-gray-100 lg:w-[80%] px-5 sm:px-10 lg:px-20 xl:px-40">
       <div className="container pt-20 pb-20 lg:pb-40 flex items-center">
@@ -197,53 +167,6 @@ const MailContact = () => {
               <Button className="bg-gray-600 text-white">送信</Button>
             </form>
           </Form>
-          {/* <form onSubmit={handleSubmit} className="space-y-10">
-        <div className="mb-3">
-          <label>お名前 :</label>
-          <input
-            type="text"
-            className="form-control block w-48 sm:w-60"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label>メールアドレス :</label>
-          <input
-            type="email"
-            className="form-control block w-72 sm:w-80 md:w-96"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label>タイトル :</label>
-          <input
-            type="text"
-            className="form-control block w-72 sm:w-full"
-            value={subject}
-            onChange={(e) => setSubject(e.target.value)}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label>メッセージ :</label>
-          <textarea
-            value={message}
-            className="form-control block w-72 sm:w-full"
-            onChange={(e) => setMessage(e.target.value)}
-            required
-          ></textarea>
-        </div>
-        <button
-          type="submit"
-          className="w-16 h-8 text-white bg-gray-700 rounded-lg transition-transform transform active:scale-95 active:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
-        >
-          送信
-        </button>
-      </form> */}
         </div>
       </div>
     </div>
